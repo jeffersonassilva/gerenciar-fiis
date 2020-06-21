f6353ad51252aa9fe7c48928607594ce1bd4cd7b
Jefferson Silva <jeffersonassilva@gmail.com>
2020-06-21 18:25:10 -0300

    feat: Adicionando a listagem de recebíveis

--- app/Services/DividendosService.php
+++ app/Services/DividendosService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Criteria\OrderByDtPagamentoCriteria;
 use App\Repositories\DividendosRepository;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Class DividendosService
@@ -36,5 +37,30 @@ class DividendosService extends AbstractService
         $data = $this->repository->with($this->repository->relationships);
 
         return request()->pagination == 'false' ? $data->all() : $data->paginate();
+    }
+
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function recebiveis()
+    {
+        return DB::table('dividendos')
+            ->select('fiis.co_sigla', 'fiis.id', 'dividendos.dt_pagamento', (
+                DB::raw("(
+                    SELECT sum(cotas.nr_cotas)
+                    FROM cotas
+                    WHERE cotas.cd_fii = dividendos.cd_fii
+                    AND cotas.dt_compra <= dividendos.dt_fechamento
+                    ) AS nr_cotas")
+            ), DB::raw("(
+                    SELECT sum(cotas.nr_cotas) * dividendos.vl_dividendo
+                    FROM cotas
+                    WHERE cotas.cd_fii = dividendos.cd_fii
+                    AND cotas.dt_compra <= dividendos.dt_fechamento
+                    ) AS vl_recebido")
+            )
+            ->join('fiis', 'fiis.id', '=', 'dividendos.cd_fii')
+            ->orderBy('dividendos.dt_pagamento', 'asc')
+            ->get();
     }
 }
